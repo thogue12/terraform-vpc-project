f9426866a59d437673e6d4e446a4fb35ccf7b79d
thogue12 <hoguetimothy12@gmail.com>
2025-02-19 21:56:48 -0500

    after a dozen plus fails i finally decided to add VPC flowlogs

---
+++ vpc-flow-logs.tf
@@ -0,0 +1,50 @@
+resource "aws_flow_log" "this" {
+  iam_role_arn    = aws_iam_role.this.arn
+  log_destination = aws_cloudwatch_log_group.this.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.tim-vpc.id
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name = "example"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "this" {
+  name   = "example"
+  role   = aws_iam_role.this.id
+  policy = data.aws_iam_policy_document.example.json
+}
